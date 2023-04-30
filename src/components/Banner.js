16cada19c9a816af069e2e4dbc333aa5fbd54ccc
Sharon Tieu <tieusharon@gmail.com>
2023-04-29 22:52:02 -0700

    Completed portfolio.

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,16 @@
 import { Container, Row, Col } from "react-bootstrap";
 import headshot from '../assets/img/headshot.png';
 import Typewriter from "typewriter-effect";
+import * as FileSaver from 'file-saver';
+// eslint-disable-next-line
+import { saveAs } from 'file-saver';
 
+const handleResumeDownload = () => {
+  FileSaver.saveAs(
+    'https://media.licdn.com/dms/document/D562DAQHCoxHf-hhj8Q/profile-treasury-document-pdf-analyzed/0/1682829277525?e=1683763200&v=beta&t=m9v3rQgUfUr4hdDA0-Uoh69Qw3dAl1i00rRofd98Rqw',
+    'Sharon Tieu_Resume'
+  );
+}
 
 export const Banner = () => {
 
@@ -28,8 +37,10 @@ export const Banner = () => {
             <button
               type="button"
               className="d-inline-flex p-2 btn btn-outline-secondary resume-btn"
-              target="_blank"
-              >Resume</button>
+              onClick={handleResumeDownload}
+              download="SharonTieu_Resume.pdf">
+              Resume
+            </button>
           </Col>
         </Row>
       </Container>
